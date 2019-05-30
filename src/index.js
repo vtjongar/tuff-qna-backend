52367094a6d4419a6055e8f453077d3fa1239f4f
Vanessa Henderson <bookvanny@gmail.com>
2019-05-29 20:09:49 -0400

    added code to get all questions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const morgan = require("morgan");
 
 const app = express();
 
+// baby database
+const questions = [];
+
 app.use(helmet());
 
 app.use(bodyParser.json());
@@ -13,3 +16,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(morgan("combined"));
+
+// this gets all existing questions
+app.get("/", (req, res) => {
+  const qs = questions.map(q => ({
+    id: q.id,
+    title: q.title,
+    description: q.description,
+    answers: q.answers.length
+  }));
+  res.send(qs);
+});
