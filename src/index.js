3419af54c380c164af51cffdbf7f92b2a9ce83d2
Vanessa Henderson <bookvanny@gmail.com>
2019-05-29 20:06:41 -0400

    added use method for dependencies

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+
+const app = express();
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(morgan("combined"));
